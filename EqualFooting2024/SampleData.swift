8e0b248437ddb2f36055ac75276a9e82aa7b59d6
Rachel Auguste <rachelauguste36@gmail.com>
2024-07-31 15:08:53 -0400

    Every single opportunity with a search bar!

---
+++ EqualFooting2024/SampleData.swift
@@ -0,0 +1,18 @@
+//
+//  SampleData.swift
+//  EqualFooting2024
+//
+//  Created by Scholar on 7/31/24.
+//
+
+import SwiftUI
+
+struct SampleData: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SampleData()
+}
