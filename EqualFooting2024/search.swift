12e887035df2d8cce8c4c748906102f77d9d095c
Joanne Lin <joannelin910@gmail.com>
2024-07-31 14:39:15 -0400

    contentView

--- EqualFooting2024/search.swift
+++ EqualFooting2024/search.swift
@@ -5,4 +5,14 @@
 //  Created by Scholar on 7/30/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct search: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    search()
+}
